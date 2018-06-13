70a9a4c5ee2bde8d86dddbc191f41b6d878d691f
Augo Gum <azgazg40@yahoo.com>
2018-06-13 15:09:18 -0400

    Less Coding then previous versions

--- csv.rb
+++ csv.rb
@@ -1,5 +1,9 @@
 require 'csv'
 
+def output_update(r_desc, r_id)
+  puts "UPDATE pp SET pp.meta_description = \"#{r_desc}\" WHERE pp.id = #{r_id};"
+end
+
 CSV.foreach('/home/augo/Documents/RubyTests/finalchange.csv') do |row|
   row_description = row[2]
   row_id = row[0]
@@ -12,31 +16,23 @@ CSV.foreach('/home/augo/Documents/RubyTests/finalchange.csv') do |row|
     row_description.gsub!('<strong>', '')
     row_description.gsub!('</strong>', '')
     row_description.gsub!('<br />', '')
-    length = row_description.length
 
-    arr = ['1296', '1297', '1298', '1299', '1300', '1346']
-    links = arr
+    html_links = %w(1296 1297 1298 1299 1300 1346)
 
-    if (links.include? (row_id))
-    row_description = ''
-    end
+    if (!html_links.include? (row_id))
 
-    def output_update(r_desc, r_id)
-      puts "UPDATE pp SET pp.meta_description = \"#{r_desc}\" WHERE pp.id = #{r_id};"
-    end
+      if (row_description.length >= 160)
+        period = (0..160).find_all {|i| row_description[i, 1] == '.'}
 
-    if (length >= 160)
-      period_find = (0..160).find_all {|i| row_description[i, 1] == '.'}
-      period = period_find
+        output_update(row_description[0..period.last], row_id) unless period.empty?
 
-      if (!period.empty?)
-        output_update(row_description[0..period.last], row_id)
+      else
+
+        output_update(row_description[0..160], row_id)
+
       end
-
-    else
-
-      output_update(row_description[0..160], row_id)
     end
   end
 end
+
 
